b70557fc6a5ca41d181fe9f02d4dbccb3feb3e5a
Sairam Pabbisetty <106012517+SairamPabbisetty@users.noreply.github.com>
2024-12-18 13:39:26 +0530

    Update connection.js

--- server/DbConnection/connection.js
+++ server/DbConnection/connection.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose")
 
 const connectDB = async() => {
     try{
-        const con = await mongoose.connect("mongodb+srv://sairampabbisetty04:sairam@04P@hotelmanagement.1egny.mongodb.net/HotelBooking?retryWrites=true&w=majority");
+        const con = await mongoose.connect("mongodb+srv://sairampabbisetty04:sairam@04P@hotelmanagement.1egny.mongodb.net/?retryWrites=true&w=majority&appName=HotelManagement
+");
         console.log("Database Connected");
     } catch(err) {
         console.log(err);
