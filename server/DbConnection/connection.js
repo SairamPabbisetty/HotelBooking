6aee8a7c6248d24070f29a43e9cf119d132b253f
SairamPabbisetty <sairampabbisetty04@gmail.com>
2024-12-18 14:25:34 +0530

    db change

--- server/DbConnection/connection.js
+++ server/DbConnection/connection.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose")
 
 const connectDB = async() => {
     try{
-        const con = await mongoose.connect("mongodb+srv://sairampabbisetty04:sairam@04P@hotelmanagement.1egny.mongodb.net/?retryWrites=true&w=majority&appName=HotelManagement
-");
+        const con = await mongoose.connect("mongodb://localhost:27017/HotelBooking");
         console.log("Database Connected");
     } catch(err) {
         console.log(err);
