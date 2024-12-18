3bb7a39744977eaa5a7768325ffd7879043674eb
Sairam Pabbisetty <106012517+SairamPabbisetty@users.noreply.github.com>
2024-12-18 13:30:10 +0530

    Update connection.js

--- server/DbConnection/connection.js
+++ server/DbConnection/connection.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose")
 
 const connectDB = async() => {
     try{
-        const con = await mongoose.connect("mongodb+srv://sairampabbisetty04:<sairam@04P>@hotelmanagement.1egny.mongodb.net/", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+        const con = await mongoose.connect("mongodb+srv://sairampabbisetty04:<sairam@04P>@hotelmanagement.1egny.mongodb.net/");
         console.log("Database Connected");
     } catch(err) {
         console.log(err);
